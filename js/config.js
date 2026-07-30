2a5eb1e6b70e7e0e6154e3b4c05b5be757e788e7
pajarr10 <fajaarr1001@gmail.com>
2026-07-30 19:15:40 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 window.NEURA = window.NEURA || {};
 
 NEURA.config = {
-  API_BASE: "http://localhost:3000",
+  API_BASE: "https://baseapiirewind.netlify.app/",
   API_KEY: "ziP11",           // JANGAN PERNAH DIUBAH
   CONTEXT_LIMIT: 16,          // jumlah pesan terakhir yang dipakai sebagai memori percakapan
   STORAGE_KEY: "neura_chat_history_v1",
